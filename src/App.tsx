5490705efcd372fbfd1afde64958d183dc76e48b
Baptiste Martinet <baptiste.martinet@epitech.eu>
2023-01-08 18:20:18 +0100

    feat: init apollo client

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,19 @@
 import { registerRootComponent } from 'expo';
+import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
 import { Provider as PaperProvider } from 'react-native-paper';
 import { Login } from 'components/screens';
 
-function App() {
-  return (
+const client = new ApolloClient({
+  uri: 'https://taale-api.herokuapp.com/', // TODO move to env
+  cache: new InMemoryCache(),
+});
+
+const App = () => (
+  <ApolloProvider client={client}>
     <PaperProvider>
       <Login />
     </PaperProvider>
-  );
-}
+  </ApolloProvider>
+);
 
 registerRootComponent(App);
