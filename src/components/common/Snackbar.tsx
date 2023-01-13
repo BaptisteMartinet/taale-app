76572e1296f0562fafafddd1487d3d1bbd499afe
Baptiste Martinet <baptiste.martinet@epitech.eu>
2023-01-13 16:56:59 +0100

    refactor(components/common): minor

--- src/components/common/Snackbar.tsx
+++ src/components/common/Snackbar.tsx
@@ -5,18 +5,17 @@ import { useTranslation } from 'react-i18next';
 import store from 'store/common/snackbar';
 
 const Snackbar = () => {
-  const { visible, msg, duration } = store;
   const { t } = useTranslation('common', { keyPrefix: 'snackbar' });
   return (
     <SnackbarPaper
-      visible={visible}
+      visible={store.visible}
       onDismiss={() => { store.dismiss(); }}
-      duration={duration}
+      duration={store.duration}
       action={{
         label: t('close'),
         onPress: () => { store.dismiss(); },
       }}>
-      {msg}
+      {store.msg}
     </SnackbarPaper>
   );
 };
