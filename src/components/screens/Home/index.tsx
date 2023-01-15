506d53fc2b6618515c8d09b62abc576ceb28ac25
Baptiste Martinet <baptiste.martinet@epitech.eu>
2023-01-15 16:09:52 +0100

    feat(components/screens): implement basic homepage FAB

--- src/components/screens/Home/index.tsx
+++ src/components/screens/Home/index.tsx
@@ -2,14 +2,21 @@ import type { RootStackParamList } from 'App';
 import type { NativeStackScreenProps } from '@react-navigation/native-stack';
 
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
+import { FAB } from 'react-native-paper';
 
 type NavigationProps = NativeStackScreenProps<RootStackParamList, 'Home'>;
 
 export default function Home(props: NavigationProps) {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <FAB
+        icon="pencil-plus"
+        style={styles.fab}
+        onPress={() => {
+          console.warn('Not handled'); //TODO FAB redirection
+        }}
+      />
     </View>
   );
 }
@@ -17,8 +24,11 @@ export default function Home(props: NavigationProps) {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    padding: 10,
+  },
+  fab: {
+    position: 'absolute',
+    right: 40,
+    bottom: 50,
   },
 });
