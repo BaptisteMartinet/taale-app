96ba2bc1bbeeabc4a6381e1894ed9e90a330af25
Baptiste Martinet <baptiste.martinet@epitech.eu>
2023-02-17 22:03:17 +0100

    feat(core/aplloClient): set device locale

--- src/core/apolloClient.ts
+++ src/core/apolloClient.ts
@@ -14,6 +14,7 @@ const authLink = setContext(async (_, { headers }) => {
     headers: {
       ...headers,
       authorization: `Bearer ${token}`,
+      'content-language': 'fr', // TODO
     }
   }
 });
