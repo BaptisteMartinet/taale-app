7208417d5f592693da64d2b53fadf0cfd7d23e6f
Baptiste Martinet <baptiste.martinet@epitech.eu>
2023-02-18 12:16:39 +0100

    feat(components/screens): update DailyStory styles

--- src/components/screens/Home/DailyStory.tsx
+++ src/components/screens/Home/DailyStory.tsx
@@ -2,7 +2,7 @@ import type { Story } from 'store/screens/home/api';
 
 import React from 'react';
 import { FlatList, Text, StyleSheet } from 'react-native';
-import { ActivityIndicator } from 'react-native-paper';
+import { ActivityIndicator, Divider } from 'react-native-paper';
 
 export interface DailyStoryProps
 {
@@ -13,17 +13,25 @@ const DailyStory = (props: DailyStoryProps) => {
   const { story } = props;
   return (
     <FlatList
+      style={styles.container}
       data={story?.sentences}
       renderItem={({ item }) => <Text style={styles.sentence}>{item.text}</Text> }
       keyExtractor={(item) => item.id.toString() }
       ListEmptyComponent={ActivityIndicator}
+      ItemSeparatorComponent={() => <Divider style={{ width: '5%' }}/>}
     />
   );
 };
 
 const styles = StyleSheet.create({
+  container: {
+    padding: 20,
+  },
   sentence: {
     fontSize: 16,
+    color: '#fafafa',
+    fontWeight: 'bold',
+    lineHeight: 32,
   },
 });
 
