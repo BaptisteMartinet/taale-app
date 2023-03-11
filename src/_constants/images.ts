ae613a7c8714554d8090316c24a4f540550d16eb
Baptiste Martinet <baptiste.martinet@epitech.eu>
2023-03-11 23:47:42 +0100

    rename onboard folder

--- src/_constants/images.ts
+++ src/_constants/images.ts
@@ -1,10 +1,10 @@
 export default {
   onboarding: {
-    slide1: require('../../assets/onboarding-carousel/slide1.png'),
-    slide2: require('../../assets/onboarding-carousel/slide2.png'),
-    slide3: require('../../assets/onboarding-carousel/slide3.png'),
-    slide4: require('../../assets/onboarding-carousel/slide4.png'),
-    slide5: require('../../assets/onboarding-carousel/slide5.png'),
-    slide6: require('../../assets/onboarding-carousel/slide6.png'),
+    slide1: require('../../assets/onboarding/slide1.png'),
+    slide2: require('../../assets/onboarding/slide2.png'),
+    slide3: require('../../assets/onboarding/slide3.png'),
+    slide4: require('../../assets/onboarding/slide4.png'),
+    slide5: require('../../assets/onboarding/slide5.png'),
+    slide6: require('../../assets/onboarding/slide6.png'),
   },
 };
