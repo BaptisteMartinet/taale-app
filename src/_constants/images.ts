f299f72a96c27db7f9699bddb25d1b59ba2685c1
Baptiste Martinet <baptiste.martinet@epitech.eu>
2023-03-11 19:37:39 +0100

    feat: constants folder

---
+++ src/_constants/images.ts
@@ -0,0 +1,10 @@
+export default {
+  onboarding: {
+    slide1: require('../../assets/onboarding-carousel/slide1.png'),
+    slide2: require('../../assets/onboarding-carousel/slide2.png'),
+    slide3: require('../../assets/onboarding-carousel/slide3.png'),
+    slide4: require('../../assets/onboarding-carousel/slide4.png'),
+    slide5: require('../../assets/onboarding-carousel/slide5.png'),
+    slide6: require('../../assets/onboarding-carousel/slide6.png'),
+  },
+};
