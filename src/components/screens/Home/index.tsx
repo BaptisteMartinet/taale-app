b76423549b121c3a4ac2703a82343f513b8321fb
Baptiste Martinet <baptiste.martinet@epitech.eu>
2023-03-12 16:35:35 +0100

    feat: add Home screen texts

--- src/components/screens/Home/index.tsx
+++ src/components/screens/Home/index.tsx
@@ -2,6 +2,7 @@ import type { RootStackParamList } from 'components/Navigator';
 import type { NativeStackScreenProps } from '@react-navigation/native-stack';
 
 import React from 'react';
+import { useTranslation } from 'react-i18next';
 import { StyleSheet, View } from 'react-native';
 import { observer } from 'mobx-react';
 import { FAB } from 'react-native-paper';
@@ -13,11 +14,13 @@ type NavigationProps = NativeStackScreenProps<RootStackParamList, 'Home'>;
 
 const Home = observer((props: NavigationProps) => {
   const { navigation } = props;
+  const { t } = useTranslation('screens', { keyPrefix: 'home' });
   return (
     <View style={styles.container}>
       <SentencesList sentences={store.dailyStory?.sentences ?? []} disableControls/>
       <FAB
         icon="pencil-plus"
+        label={t('participate')}
         style={styles.fab}
         onPress={() => {
           if (accountStore.user !== null)
--- src/lang/translation/screens/index.ts
+++ src/lang/translation/screens/index.ts
@@ -1,23 +1,27 @@
 import login, { type LoginScreenDefinition } from './login';
 import register, { type RegisterScreenDefinition } from './register';
 import onboarding, { type OnboardingScreenDefinition } from './onboarding';
+import home, { type HomeScreenDefinition } from './home';
 
 export type ScreensDefinition = {
   login: LoginScreenDefinition,
   register: RegisterScreenDefinition,
   onboarding: OnboardingScreenDefinition,
+  home: HomeScreenDefinition,
 }
 
 const fr: ScreensDefinition = {
   login: login.fr,
   register: register.fr,
   onboarding: onboarding.fr,
+  home: home.fr,
 };
 
 const en: ScreensDefinition = {
   login: login.en,
   register: register.en,
   onboarding: onboarding.en,
+  home: home.en,
 };
 
 export default {
