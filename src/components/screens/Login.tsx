fa02ba524b4013e1c1f773e2b7178d1e7196400e
Baptiste Martinet <baptiste.martinet@epitech.eu>
2023-03-18 23:53:45 +0100

    feat(components/screens): implement formik on Login screen

--- src/components/screens/Login.tsx
+++ src/components/screens/Login.tsx
@@ -4,10 +4,16 @@ import type { NativeStackScreenProps } from '@react-navigation/native-stack';
 import React from 'react';
 import { StyleSheet, View, Keyboard } from 'react-native';
 import { TextInput, Button, Text } from 'react-native-paper';
-import { Formik } from 'formik';
 import { useTranslation } from 'react-i18next';
+import { Formik } from 'formik';
+import * as Yup from 'yup';
 import { handleWithSnack } from 'core/utils/promises';
 import store from 'store/common/account';
+
+const LoginValidationSchema = Yup.object().shape({
+  email: Yup.string().email().required(),
+  password: Yup.string().required(),
+});
 
 type NavigationProps = NativeStackScreenProps<RootStackParamList, 'Login'>;
 
@@ -21,6 +27,7 @@ const Login = (props: NavigationProps) => {
           email: '',
           password: '',
         }}
+        validationSchema={LoginValidationSchema}
         onSubmit={values => {
           Keyboard.dismiss();
           const promise = store.login(values);
@@ -31,34 +38,37 @@ const Login = (props: NavigationProps) => {
           });
         }}
       >
-        {({ handleChange, handleSubmit, values, isSubmitting }) => (
+        {({ handleChange, handleSubmit, values, errors, touched, isSubmitting }) => (
           <View style={styles.formContainer}>
             <View style={styles.headline}>
               <Text variant="headlineLarge">{t('form.headlineTitle')}</Text>
               <Text variant="titleSmall">{t('form.headlineDescription')}</Text>
             </View>
             <TextInput
-              label={t<string>('form.email')}
+              label={t('form.email')}
               mode="outlined"
               keyboardType="email-address"
               right={<TextInput.Icon icon="email" />}
               style={styles.textField}
               value={values.email}
               onChangeText={handleChange('email')}
+              error={Boolean(errors.email) && touched.email}
             />
             <TextInput
-              label={t<string>('form.password')}
+              label={t('form.password')}
               mode="outlined"
               secureTextEntry
               right={<TextInput.Icon icon="lock" />}
               style={styles.textField}
               value={values.password}
               onChangeText={handleChange('password')}
+              error={Boolean(errors.password) && touched.password}
             />
             <Button
               mode="contained"
               style={styles.button}
               disabled={isSubmitting}
+              loading={isSubmitting}
               onPress={() => { handleSubmit(); }}
             >
               {t('form.submit')}
