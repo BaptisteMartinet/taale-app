4f1fcec25f874ea973e566e2135a3e03addbadfe
Baptiste Martinet <baptiste.martinet@epitech.eu>
2023-03-18 18:59:17 +0100

    feat: implement NewSentenceForm with texts

--- src/components/screens/PartialStory/NewSentenceForm.tsx
+++ src/components/screens/PartialStory/NewSentenceForm.tsx
@@ -1,26 +1,61 @@
+import type { NativeStackNavigationProp } from '@react-navigation/native-stack';
+import type { RootStackParamList } from 'components/Navigator';
+
 import React from 'react';
-import { StyleSheet, View } from 'react-native';
+import { StyleSheet, View, Keyboard } from 'react-native';
 import { IconButton, TextInput } from 'react-native-paper';
 import { useTranslation } from 'react-i18next';
+import { Formik } from 'formik';
+import { useNavigation } from '@react-navigation/native';
+import { handleWithSnack } from 'core/utils/promises';
+import sentenceStore from 'store/common/sentence';
+import store from 'store/screens/partial-story';
 
 const NewSentenceForm = () => {
-  const { t} = useTranslation('screens', { keyPrefix: 'partialStory' });
+  const { t } = useTranslation('screens', { keyPrefix: 'partialStory' });
+  const navigation = useNavigation<NativeStackNavigationProp<RootStackParamList>>();
   return (
-    <View style={styles.container}>
-      <TextInput
-        style={styles.textInput}
-        mode="outlined"
-        placeholder={t('write')}
-        multiline
-      />
-      <View style={styles.iconWrapper}>
-        <IconButton
-          style={styles.icon}
-          icon="send"
-          onPress={() => { console.log('sending...'); }}
-        />
-      </View>
-    </View>
+    <Formik
+      initialValues={{ text: '' }}
+      onSubmit={(values, actions) => {
+        const { text } = values;
+        const { lastSentence } = store;
+        if (!lastSentence)
+          return;
+        const promise = sentenceStore.create({ text, parentSentenceId: lastSentence.id });
+        handleWithSnack(promise, {
+          successMessage: t('writeSuccess'),
+          onSuccess: () => {
+            actions.resetForm();
+            Keyboard.dismiss();
+            store.refresh().catch((e) => console.error(e));
+            //navigation.replace('PartialStory'); TODO redirect to action page
+          },
+          errorMessage: true,
+        });
+      }}
+    >
+      {({ handleSubmit, handleChange, isSubmitting, values }) => (
+        <View style={styles.container}>
+          <TextInput
+            style={styles.textInput}
+            mode="outlined"
+            placeholder={t('write')}
+            multiline
+            value={values.text}
+            onChangeText={handleChange('text')}
+          />
+          <View style={styles.iconWrapper}>
+            <IconButton
+              style={styles.icon}
+              icon="send"
+              disabled={isSubmitting}
+              onPress={() => handleSubmit()}
+            />
+          </View>
+        </View>
+      )}
+    </Formik>
   );
 };
 
--- src/lang/translation/screens/partialStory.ts
+++ src/lang/translation/screens/partialStory.ts
@@ -1,13 +1,16 @@
 export type PartialStoryScreenDefinition = {
   write: string,
+  writeSuccess: string,
 }
 
 const fr: PartialStoryScreenDefinition = {
   write: "Écrire",
+  writeSuccess: "Enregistré avec succès",
 };
 
 const en: PartialStoryScreenDefinition = {
   write: "Write",
+  writeSuccess: "Successfully saved",
 };
 
 export default {
