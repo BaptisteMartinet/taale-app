4f9e6b3e7788699e8e65284f154947b89031db71
Baptiste Martinet <baptiste.martinet@epitech.eu>
2023-03-25 16:07:50 +0100

    feat(components/screens): implement StoryViewer store refresh on focus with route params

--- src/components/screens/StoryViewer.tsx
+++ src/components/screens/StoryViewer.tsx
@@ -4,31 +4,31 @@ import type { RootStackParamList } from 'components/Navigator';
 import React from 'react';
 import { StyleSheet, SafeAreaView } from 'react-native';
 import { useFocusEffect } from '@react-navigation/native';
+import { observer } from 'mobx-react';
 import { handleWithSnack } from 'core/utils/promises';
 import { SentencesList } from 'components/common';
 import store from 'store/screens/story-viewer';
 
 type NavigationProps = NativeStackScreenProps<RootStackParamList, 'StoryViewer'>;
 
-const StoryViewer = (props: NavigationProps) => {
+const StoryViewer = observer((props: NavigationProps) => {
   const { storyId } = props.route.params;
-  useFocusEffect(() => {
+  useFocusEffect(
     React.useCallback(() => {
       const promise = store.refresh(storyId);
       handleWithSnack(promise, { successMessage: null, errorMessage: true });
     }, [storyId])
-  });
+  );
   return (
     <SafeAreaView style={styles.container}>
       <SentencesList sentences={store.story?.sentences ?? []} disableControls />
     </SafeAreaView>
   );
-};
+});
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#2b2b2b',
   },
 });
 
