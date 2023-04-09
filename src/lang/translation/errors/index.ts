c0bd3c9b45056fb9305da21fbe539225b1056166
Baptiste Martinet <baptiste.martinet@epitech.eu>
2023-04-09 11:56:25 +0200

    feat(lang/translation): rework global errors translations

--- src/lang/translation/errors/index.ts
+++ src/lang/translation/errors/index.ts
@@ -1,39 +1,51 @@
 export type ErrorsDefinition = {
   _Default: string,
   //Client errors
-  InvalidPassword: string,
+  InvalidLoginOrPassword: string,
+  InvalidEmailFormat: string,
   EmailTaken: string,
+  InvalidEmailValidationCode: string,
+  InvalidUsernameLength: string,
+  InvalidUsernameChar: string,
   UsernameTaken: string,
+  InvalidSentenceLength: string,
   ResourceNotFound: string,
   SpamDetected: string,
   InsufficientPermission: string,
-
   // Server errors
 };
 
 const fr: ErrorsDefinition = {
-  _Default: 'Une erreur est survenue',
+  _Default: "Une erreur est survenue",
   //Client errors
-  InvalidPassword: 'Mot de passe invalide',
-  EmailTaken: 'Adresse email déjà utilisé',
+  InvalidLoginOrPassword: "Nom d'utilisateur ou mot de passe invalide",
+  InvalidEmailFormat: "Format d'email invalide",
+  EmailTaken: "Adresse email déjà utilisée",
+  InvalidEmailValidationCode: "Code de vérification d'email invalide",
+  InvalidUsernameLength: "Nom d'utilisateur trop court/long",
+  InvalidUsernameChar: "Le nom d'utilisateur contient un caractère invalide",
   UsernameTaken: "Nom d'utilisateur déjà utilisé",
-  ResourceNotFound: 'Resource non trouvée',
-  SpamDetected: 'Spam détecté',
-  InsufficientPermission: 'Permissions insufisantes',
-
+  InvalidSentenceLength: "Taille de paragraphe invalide",
+  ResourceNotFound: "Resource non trouvée",
+  SpamDetected: "Spam détecté",
+  InsufficientPermission: "Permissions insufisantes",
   // Server errors
 } as const;
 
 const en: ErrorsDefinition = {
-  _Default: 'An error has occured',
+  _Default: "An error has occured",
   //Client errors
-  InvalidPassword: 'Invalid password',
-  EmailTaken: 'Email already taken',
+  InvalidLoginOrPassword: "Invalid login or password",
+  InvalidEmailFormat: "Invalid email format",
+  EmailTaken: "Email already taken",
+  InvalidEmailValidationCode: "Invalid email verification code",
+  InvalidUsernameLength: "Username too short/long",
+  InvalidUsernameChar: "Username contains an invalid character",
   UsernameTaken: "Username already taken",
-  ResourceNotFound: 'Resource not found',
-  SpamDetected: 'Spam detected',
-  InsufficientPermission: 'Insufficient permission',
-
+  InvalidSentenceLength: "Invalid sentence length",
+  ResourceNotFound: "Resource not found",
+  SpamDetected: "Spam detected",
+  InsufficientPermission: "Insufficient permission",
   // Server errors
 } as const;
 
