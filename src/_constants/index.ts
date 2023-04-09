e29c4bc344d2734b67175f8e335bd5470c3ed43c
Baptiste Martinet <baptiste.martinet@epitech.eu>
2023-04-09 16:29:15 +0200

    feat(_constants): implement vars constatns

--- src/_constants/index.ts
+++ src/_constants/index.ts
@@ -1,3 +1,4 @@
 export { default as Images } from './images';
 export * from './app';
+export * from './vars';
 export * from './misc';
---
+++ src/_constants/vars.ts
@@ -0,0 +1,7 @@
+export const EmailVerificationCodeLength = 4;
+export const UsernameMinLength = 2;
+export const UsernameMaxLength = 24;
+export const PasswordMinLength = 4;
+
+export const SentenceTextMinLength = 3;
+export const SentenceTextMaxLength = 280;
