8c31774ac5ade2b494bc845ff89ac10db4562bd6
Baptiste Martinet <baptiste.martinet@epitech.eu>
2023-04-09 11:42:22 +0200

    feat(store/common): implement emailValidationCode args on register mutation

--- src/store/common/account/api/register.ts
+++ src/store/common/account/api/register.ts
@@ -2,10 +2,10 @@ import { gql } from '@apollo/client';
 import apolloClient from 'core/apolloClient';
 
 const mutation = gql`
-mutation Register($username: String!, $email: String!, $password: String!) {
+mutation Register($username: String!, $email: String!, $password: String!, $emailValidationCode: String!) {
   public {
     account {
-      register(username: $username, email: $email, password: $password)
+      register(username: $username, email: $email, password: $password, emailValidationCode: $emailValidationCode)
     }
   }
 }
@@ -15,6 +15,7 @@ export interface RegisterVariables {
   username: string;
   email: string;
   password: string;
+  emailValidationCode: string;
 }
 
 export default function Register(variables: RegisterVariables) {
