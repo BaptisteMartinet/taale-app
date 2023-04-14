ed4ae18da7af24a9407628f7fc5012ff105d0e63
Baptiste Martinet <baptiste.martinet@epitech.eu>
2023-04-15 00:15:57 +0200

    feat(core/utils): use ObservablePromiseStatus in Fetchable

--- src/core/utils/Fetchable.ts
+++ src/core/utils/Fetchable.ts
@@ -1,98 +1,28 @@
-import { action, computed, makeObservable, observable } from 'mobx';
+import type { TrackOptions } from './ObservablePromiseStatus';
 
-export type FetchableStatus = 'uninitialized' | 'pending' | 'success' | 'error';
+import ObservablePromiseStatus from './ObservablePromiseStatus';
 
-export interface FetchableOpts<ResultType> {
-  thenUnhandled?: (result: ResultType) => void,
-  catchUnhandled?: (error: Error) => void,
-}
-
-class Fetchable<ArgsType extends any[], ResultType> {
-  public result: ResultType | undefined = undefined;
-  public lastResult: ResultType | undefined = undefined;
-  public error: Error | undefined = undefined;
-  public lastError: Error | undefined = undefined;
-  public status: FetchableStatus = 'uninitialized';
-
-  constructor(private fetch: (...args: ArgsType) => Promise<ResultType>, private opts: FetchableOpts<ResultType> = {}) {
-    makeObservable(this, {
-      result: observable,
-      lastResult: observable,
-      error: observable,
-      lastError: observable,
-      status: observable,
-      setResult: action,
-      setError: action,
-      setStatus: action,
-      loading: computed,
-      reset: action,
-    });
-  }
-
-  public setResult(result: ResultType) {
-    this.result = result;
-    this.lastResult = result;
-    this.error = undefined;
-    this.lastError = undefined;
-    this.status = 'success';
-  }
-
-  public setError(error: Error) {
-    this.error = error;
-    this.lastError = error;
-    this.result = undefined;
-    this.lastResult = undefined;
-    this.status = 'error';
-  }
-
-  public setStatus(status: FetchableStatus) {
-    this.status = status;
-    if (status === 'pending') {
-      this.result = undefined;
-      this.error = undefined;
-    }
-  }
-
-  public get loading() {
-    return this.status === 'pending';
+class Fetchable<ArgsType extends any[], ResultType> extends ObservablePromiseStatus<ResultType> {
+  constructor(private fetch: (...args: ArgsType) => Promise<ResultType>, private opts: TrackOptions<ResultType>) {
+    super();
   }
 
   /**
-   * Resets the Fetchable to its uninitialized state
-   */
-  public reset() {
-    this.result = undefined;
-    this.lastResult = undefined;
-    this.error = undefined;
-    this.lastError = undefined;
-    this.status = 'uninitialized';
-  }
-
-  /**
-   * Returns the last result, reloads otherwise.
+   * Returns the last fulfilled result, reloads otherwise.
    */
   public ensureSuccess(...args: ArgsType) {
-    if (this.status === 'success')
+    if (this.status === 'fulfilled')
       return this.result as ResultType;
     return this.ensureSuccessReload(...args);
   }
 
   /**
    * Reloads and returns the result.
-   * Does not handle race conditions between reloads
    */
   public async ensureSuccessReload(...args: ArgsType) {
-    const { thenUnhandled, catchUnhandled } = this.opts;
-    this.setStatus('pending');
-    return this.fetch(...args).then((res => {
-      this.setResult(res);
-      thenUnhandled?.(res);
-      return res;
-    })).catch(error => {
-      this.setError(error);
-      catchUnhandled?.(error);
-      throw error;
-    });
+    const promise = this.fetch(...args);
+    this.track(promise, this.opts);
+    return promise;
   }
 }
 
