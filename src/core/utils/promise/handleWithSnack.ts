ae84bde49589a5d75b83bec96b66b86663ca927a
Baptiste Martinet <baptiste.martinet@epitech.eu>
2023-04-16 12:26:49 +0200

    feat(core/utils): improve handleWithSnack typings

--- src/core/utils/promise/handleWithSnack.ts
+++ src/core/utils/promise/handleWithSnack.ts
@@ -2,20 +2,21 @@ import { isApolloError } from '@apollo/client';
 import i18n from 'lang';
 import snackbarStore from 'store/common/snackbar';
 
-export interface Opts {
-  onSuccess?: (value: unknown) => void;
+export interface Opts<T> {
+  onSuccess?: (value: T) => void;
   successMessage: string | null;
   onError?: (error: unknown) => void;
   errorMessage: string | true | null;
 };
 
-export default function handleWithSnack(promise: Promise<unknown>, opts: Opts) {
+export default function handleWithSnack<T>(promise: Promise<T>, opts: Opts<T>) {
   const { onSuccess, successMessage, onError, errorMessage } = opts;
   return promise.then((value) => {
     if (onSuccess)
       onSuccess(value);
     if (successMessage)
       snackbarStore.display(successMessage);
+    return value;
   }).catch((error) => {
     if (onError)
       onError(error);
