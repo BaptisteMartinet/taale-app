dc4b43e1f804e753f0243bf7b75ba1cfb93128a7
Baptiste Martinet <baptiste.martinet@epitech.eu>
2023-04-17 22:32:17 +0200

    feat(components/screens): implement SettingsSheet base

---
+++ src/components/screens/Home/SettingsSheet.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { View } from 'react-native';
+import { Text } from 'react-native-paper';
+
+const SettingsSheet = () => {
+  return (
+    <View>
+      <Text style={{ color: 'black' }}>{'hello\nhello\nhello\nhello\nhello\nhello\n'}</Text>
+    </View>
+  );
+};
+
+export default SettingsSheet;
