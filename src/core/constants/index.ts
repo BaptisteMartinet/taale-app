175eac3cecc5dc0d927068916c977c38e2dfe371
Baptiste Martinet <baptiste.martinet@epitech.eu>
2023-04-19 20:06:47 +0200

    feat(core/constants): add baptechWebsiteURL constant

--- src/core/constants/index.ts
+++ src/core/constants/index.ts
@@ -1,3 +1,4 @@
 export * from './app';
 export * from './vars';
+export * from './links';
 export { default as Images } from './images';
---
+++ src/core/constants/links.ts
@@ -0,0 +1,1 @@
+export const baptechWebsiteURL = 'https://www.baptech.fr/';
