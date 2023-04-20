dd72ba82e32342ec91eeebe42ee016297cf91837
Baptiste Martinet <baptiste.martinet@epitech.eu>
2023-04-20 23:09:00 +0200

    feat(components/screens): sow logout and account deletion buttons only when user is logged out

--- src/components/screens/Home/SettingsSheet.tsx
+++ src/components/screens/Home/SettingsSheet.tsx
@@ -53,52 +53,54 @@ const SettingsSheet = observer(() => {
             }).catch(console.error);
           }}
         />
-        <List.Item
-          title={t('logout')}
-          left={props => <List.Icon {...props} icon="logout" />}
-          disabled={accountStore.loggedOut}
-          onPress={() => {
-            const promise = accountStore.logout();
-            handleWithSnack(promise, {
-              successMessage: t('logoutSuccess'),
-              onSuccess: store.settingsOpenState.close,
-              errorMessage: true,
-            });
-          }}
-        />
+        {accountStore.loggedIn &&
+          <List.Item
+            title={t('logout')}
+            left={props => <List.Icon {...props} icon="logout" />}
+            onPress={() => {
+              const promise = accountStore.logout();
+              handleWithSnack(promise, {
+                successMessage: t('logoutSuccess'),
+                onSuccess: store.settingsOpenState.close,
+                errorMessage: true,
+              });
+            }}
+          />
+        }
       </List.Section>
 
-      <List.Section style={{ borderWidth: 1, borderColor: 'rgba(255, 77, 77, .5)', marginTop: 20 }}>
-        <List.Subheader>{t('dangerZone')}</List.Subheader>
-        <List.Item
-          title={t('deleteAccount')}
-          left={props => <List.Icon {...props} icon="delete" />}
-          disabled={accountStore.loggedOut}
-          onPress={() => {
-            Alert.alert(
-              t('accountDeletionAlert.title'),
-              t('accountDeletionAlert.description'),
-              [
-                {
-                  text: t('accountDeletionAlert.cancel'),
-                  style: 'cancel'
-                },
-                {
-                  text: t('accountDeletionAlert.submit'),
-                  style: 'destructive',
-                  onPress: () => {
-                    const promise = accountStore.deleteAccount();
-                    handleWithSnack(promise, {
-                      successMessage: t('accountDeletionSuccess'),
-                      errorMessage: true,
-                    });
-                  }
-                },
-              ],
-            );
-          }}
-        />
-      </List.Section>
+      {accountStore.loggedIn &&
+        <List.Section style={{ borderWidth: 1, borderColor: 'rgba(255, 77, 77, .5)', marginTop: 20 }}>
+          <List.Subheader>{t('dangerZone')}</List.Subheader>
+          <List.Item
+            title={t('deleteAccount')}
+            left={props => <List.Icon {...props} icon="delete" />}
+            onPress={() => {
+              Alert.alert(
+                t('accountDeletionAlert.title'),
+                t('accountDeletionAlert.description'),
+                [
+                  {
+                    text: t('accountDeletionAlert.cancel'),
+                    style: 'cancel'
+                  },
+                  {
+                    text: t('accountDeletionAlert.submit'),
+                    style: 'destructive',
+                    onPress: () => {
+                      const promise = accountStore.deleteAccount();
+                      handleWithSnack(promise, {
+                        successMessage: t('accountDeletionSuccess'),
+                        errorMessage: true,
+                      });
+                    }
+                  },
+                ],
+              );
+            }}
+          />
+        </List.Section>
+      }
     </BottomSheet>
   );
 });
