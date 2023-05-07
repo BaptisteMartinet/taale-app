f6f7f8c894366b83249764c1261c198ec8da0706
Baptiste Martinet <baptiste.martinet@epitech.eu>
2023-05-07 11:24:06 +0200

    feat(core/constants): update EmailVerificationCode and ResetPasswordCode lengths

--- src/core/constants/vars.ts
+++ src/core/constants/vars.ts
@@ -1,4 +1,4 @@
-export const EmailVerificationCodeLength = 4;
+export const EmailVerificationCodeLength = 6;
 export const UsernameMinLength = 2;
 export const UsernameMaxLength = 24;
 export const UsernameValidationRegex = new RegExp(`^[\\w\\.]{${UsernameMinLength},${UsernameMaxLength}}$`);
@@ -7,4 +7,4 @@ export const PasswordMinLength = 4;
 export const SentenceTextMinLength = 3;
 export const SentenceTextMaxLength = 180;
 
-export const ResetPasswordCodeLength = 4;
+export const ResetPasswordCodeLength = 8;
