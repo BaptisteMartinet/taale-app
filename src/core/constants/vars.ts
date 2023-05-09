0be09b029d59a1c3fc73f1890949da1cc544158a
Baptiste Martinet <baptiste.martinet@epitech.eu>
2023-05-09 21:09:15 +0200

    feat(core/constants): disallow dots in usernames

--- src/core/constants/vars.ts
+++ src/core/constants/vars.ts
@@ -1,7 +1,7 @@
 export const EmailVerificationCodeLength = 6;
 export const UsernameMinLength = 2;
 export const UsernameMaxLength = 24;
-export const UsernameValidationRegex = new RegExp(`^[\\w\\.]{${UsernameMinLength},${UsernameMaxLength}}$`);
+export const UsernameValidationRegex = new RegExp(`^[\\w]{${UsernameMinLength},${UsernameMaxLength}}$`);
 export const PasswordMinLength = 4;
 
 export const SentenceTextMinLength = 3;
