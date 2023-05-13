102232ff65c5b60d8cbd4b172f911fc5fbf7a418
Baptiste Martinet <baptiste.martinet@epitech.eu>
2023-05-13 18:25:39 +0200

    feat(components): implement AppContainer

---
+++ src/components/AppContainer.tsx
@@ -0,0 +1,35 @@
+import type { PropsWithChildren } from 'react';
+
+import * as React from 'react';
+import { StyleSheet, View } from 'react-native';
+import { isMobile } from 'core/utils/device';
+
+const WebContainer = (props: PropsWithChildren<{}>) => {
+  const { children } = props;
+  if (isMobile)
+    return <React.Fragment>{children}</React.Fragment>;
+  return (
+    <View style={styles.container}>
+      <View style={styles.appWrapper}>
+        {children}
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#455d7a',
+  },
+  appWrapper: {
+    width: 360,
+    height: 'auto',
+    aspectRatio: 6 / 13,
+    margin: 'auto',
+    overflow: 'hidden',
+    borderRadius: 32,
+  },
+});
+
+export default WebContainer;
