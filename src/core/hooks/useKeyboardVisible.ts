6c397579a5d671cb46feaa51b8d6853465e3f773
Baptiste Martinet <baptiste.martinet@epitech.eu>
2023-05-31 23:44:06 +0200

    feat(core/hooks): handle useKeyboardVisible anroid support

--- src/core/hooks/useKeyboardVisible.ts
+++ src/core/hooks/useKeyboardVisible.ts
@@ -1,11 +1,15 @@
 import React from 'react';
-import { Keyboard } from 'react-native';
+import { Platform, Keyboard } from 'react-native';
+
+// Note: keyboardWill[Show/Hide] events are not supported on Android
+const ShowEventName = Platform.OS === 'android' ? 'keyboardDidShow' : 'keyboardWillShow';
+const HideEventName = Platform.OS === 'android' ? 'keyboardDidHide' : 'keyboardWillHide';
 
 export default function useKeyboardVisible() {
   const [isKeyboardVisible, setKeyboardVisible] = React.useState(false);
   React.useEffect(() => {
-    const showListener = Keyboard.addListener('keyboardWillShow', () => setKeyboardVisible(true));
-    const hideListener = Keyboard.addListener('keyboardWillHide', () => setKeyboardVisible(false));
+    const showListener = Keyboard.addListener(ShowEventName, () => setKeyboardVisible(true));
+    const hideListener = Keyboard.addListener(HideEventName, () => setKeyboardVisible(false));
     return () => {
       showListener.remove();
       hideListener.remove();
